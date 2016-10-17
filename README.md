fed22e392cdb03542e63ebbc8dc8fade5b4ec2d6
igroz777 <igroz777@gmail.com>
2016-10-17 18:18:40 +0100

    Added a class for creating OR logic gates. Added more tests. Added info to readme file.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ###Overview
-This module exposes a class, ANDgate, that can be used to compute logical operations of several and gates connected to each other 
+This module exposes an object called `collection` that can be used to create an AND logic gate or an OR logic gate which can be combined together to compute the outcome of a logic gate circuit.
 ###Spec
-- ANDgate creates a basic gate with unbounded inputs
+- ANDgate creates a basic AND logic gate with unbounded inputs
+- ORgate creates a basic AND logic gate with unbounded inputs
 - Inputs can be set to either logical inputs(0 or 1) or to the output of another gate whose own inputs will e used for the input signal
 - Connections can be unset using the disconnectPinX method
--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-var ANDgate = require('./lib/logic-gate.js');
+var collection = require('./lib/logic-gate.js');
 console.log("running");
 
 
 'use strict';
-describe("ANDGate Class: Create an AND gate", function() {
+describe("Testing the ANDgate and ORgate classes for functionality ", function() {
 
   it("The AND logic gate should be a type of `object`, and an instance of the `ANDGate` class", function() {
-    var gate1 = new ANDgate('AND gate 1');
+    var gate1 = new collection.ANDgate('AND gate 1');
     expect(typeof gate1).to.equal(typeof {});
-    expect(gate1 instanceof ANDgate).to.be.true;
+    expect(gate1 instanceof collection.ANDgate).to.be.true;
   });
 
   it("The AND logic gate should have its input pins set to undefined at initialization", function() {
-    var gate1  = new ANDgate('AND gate 1');
+    var gate1  = new collection.ANDgate('AND gate 1');
     expect(gate1.pinA).to.equal(undefined);
     expect(gate1.pinB).to.equal(undefined);
   });
 
   it("The AND logic gate should have its pins properly defined", function() {
-    var gate1  = new ANDgate('AND gate 1');
+    var gate1  = new collection.ANDgate('AND gate 1');
     gate1.setPinA(0);
     gate1.setPinB(0);
 
@@ -30,10 +30,42 @@ describe("ANDGate Class: Create an AND gate", function() {
   });
 
   it("The AND logic gate should output 0 for inputs 0 and 1", function() {
-    var gate1  = new ANDgate('AND gate 1');
+    var gate1  = new collection.ANDgate('AND gate 1');
     gate1.setPinA(1);
     gate1.setPinB(0);
     expect(gate1.getOutput()).to.equal(0);
-    
+  });
+
+  it("The OR logic gate should have its pins properly defined", function() {
+    var gate1  = new collection.ORgate('OR gate 1');
+    gate1.setPinA(1);
+    gate1.setPinB(0);
+
+    expect(gate1.pinA).to.equal(1);
+    expect(gate1.pinB).to.equal(0);
+  });
+
+  it("The OR logic gate should output 1 for inputs 0 and 1", function() {
+    var gate1  = new collection.ORgate('OR gate 1');
+    gate1.setPinA(1);
+    gate1.setPinB(0);
+    expect(gate1.getOutput()).to.equal(1);
+  });
+
+  it("Two OR gates with input pairs [0, 1] and [1, 0] with their inputs connected to an AND gate. Expected output is 1", function() {
+    var orGate1  = new collection.ORgate('OR gate 1');
+    var orGate2  = new collection.ORgate('OR gate 2');
+    var andGate = new collection.ANDgate('AND gate');
+
+    orGate1.setPinA(0);
+    orGate1.setPinB(1);
+
+    orGate2.setPinA(1);
+    orGate2.setPinB(0);
+
+    andGate.setPinA(orGate1);
+    andGate.setPinB(orGate2);
+
+    expect(andGate.getOutput()).to.equal(1);
   });
 });
